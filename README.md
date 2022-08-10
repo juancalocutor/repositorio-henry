368fa7698b4d88356c62c7474d2178fbb32f9736
juancalocutor <juancalocutor@gmail.com>
2022-08-09 20:11:49 -0500

    archivos modificados

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # repositorio-henry
+
 Carpeta de pruebas del curso HENRY. HomeWork 01a-Git
---
+++ archivo-ejemplo.js
@@ -0,0 +1,6 @@
+function imprime1aN(n) {
+    for (var i = 1; i <= n; i++) {
+        console.log(i);
+    }
+}
+
